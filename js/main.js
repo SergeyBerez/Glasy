03f86fc36beff857bba49a17c7a6679b4c5a79ef
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2019-12-28 13:34:11 +0200

    add giles

---
+++ js/main.js
@@ -0,0 +1,46 @@
+let li = document.querySelector('.login-link');
+let modal = document.querySelector('.modal.modal-login ');
+li.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  console.log(modal);
+  if (modal.style.display == 'block') {
+    modal.style.display = 'none';
+  } else {
+    modal.style.display = 'block';
+  }
+});
+
+let contButton = document.querySelectorAll('.contacts-button');
+let modalMap = document.querySelector('.modal-map');
+
+contButton[0].onclick = function(e) {
+  e.preventDefault();
+  if (modalMap.style.display == 'block') {
+    modalMap.style.display = 'none';
+  } else {
+    modalMap.style.display = 'block';
+  }
+};
+
+let but = document.querySelectorAll('.modal-close');
+for (let i = 0; i < but.length; i++) {
+  but[i].addEventListener('click', function(e) {
+    modal.style.display = 'none';
+    modalMap.style.display = 'none';
+  });
+}
+
+setInterval(function() {
+  loginLight(li);
+}, 1000);
+function loginLight(elem) {
+  if (elem.style.background == 'grey') {
+    elem.style.background = 'black';
+  } else {
+    elem.style.background = 'grey';
+    elem.style.borderRadius = '20px';
+  }
+}
+
+console.log(11);
---
+++ js/mainglasy.js
@@ -0,0 +1,22 @@
+let slider = document.querySelector('.slides');
+let arrImg = [
+  'url(/img/Group1aaa.png)',
+  'url(/img/Group2bbb.png)',
+  'url(/img/Group3ddd.png)',
+];
+let radio = document.querySelectorAll('[type="radio"]');
+
+for (let i = 0; i < radio.length; i++) {
+  radio[i].addEventListener('click', function(e) {
+    console.log(this.dataset.type);
+    if (e.target.dataset.type) {
+      slider.style.backgroundImage = `url(/img/${e.target.dataset.type})`;
+    }
+    // if (this.id == 'slide2') {
+    //   slider.style.backgroundImage = arrImg[1];
+    // }
+    // if (this.id == 'slide3') {
+    //   slider.style.backgroundImage = arrImg[2];
+    // }
+  });
+}
