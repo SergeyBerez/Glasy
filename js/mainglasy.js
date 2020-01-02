cccd9ef7fe6b63144c3f6285a024fbb4e99bc9e1
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2020-01-02 17:42:05 +0200

    1

--- js/mainglasy.js
+++ js/mainglasy.js
@@ -17,6 +17,8 @@ for (let i = 0; i < radio.length; i++) {
     // }
   });
 }
+
+let i = 0;
 function changebackground() {
   let arrImg = [
     'url(./img/Group1aaa.png)',
@@ -24,21 +26,26 @@ function changebackground() {
     'url(./img/Group3ddd.png)',
   ];
   let arrColor = ['#849d8f', '#8996a6', '#9d8b84'];
-  let i = 0;
-  setInterval(function() {
-    console.log(radio[i].dataset.type);
-    slider.style.backgroundImage = arrImg[i];
-    slider.style.backgroundColor = arrColor[i];
-    radio[i].checked = true;
-    i++;
-    if (i >= arrImg.length) {
-      i = 0;
-    }
+  slider.style.backgroundImage = arrImg[i];
+  slider.style.backgroundColor = arrColor[i];
+  radio[i].checked = true;
+  i++;
+  if (i >= arrImg.length) {
+    i = 0;
+  }
+  // setInterval(function() {
+  //   console.log(radio[i].dataset.type);
+  //   slider.style.backgroundImage = arrImg[i];
+  //   slider.style.backgroundColor = arrColor[i];
+  //   radio[i].checked = true;
+  //   i++;
+  //   if (i >= arrImg.length) {
+  //     i = 0;
+  //   }
 
-    // for (let i = 0; i < radio.length; i++) {
-    //   console.log(radio[i].dataset.type);
-    // }
-    // slider.style.backgroundColor = e.target.dataset.color;
-  }, 2500);
+  // }, 2500);
 }
-changebackground();
+
+setInterval(function() {
+  changebackground();
+}, 2500);
