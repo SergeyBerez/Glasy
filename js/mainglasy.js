98cff98a718c1e2ddbaa2f8fba438e75f3eea42c
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2020-01-07 18:44:05 +0200

    1

--- js/mainglasy.js
+++ js/mainglasy.js
@@ -62,8 +62,6 @@ headerMenuIcon.addEventListener('click', function(e) {
 closemenu.addEventListener('click', function(e) {
   headerNav.style.left = '-490px';
   headerMenuIcon.style.opacity = '1';
-
-  console.log(1);
 });
 window.addEventListener('resize', function(e) {
   headerNav.style.display = '';
@@ -73,26 +71,32 @@ let find = document.querySelector('.nav-user_find');
 let enter = document.querySelector('.nav-user_enter');
 let modalFind = document.querySelector('.modal-find');
 let modalLogin = document.querySelector('.modal-login');
-find.addEventListener('click', function(e) {
-  console.log(e.target);
-  if (modalFind.style.display == 'block') {
+
+// меняем событие на модальных оканах показываем и скрываем
+document.addEventListener('click', function(e) {
+  if (!(e.target.tagName == 'INPUT' || e.target.tagName == 'FORM')) {
     modalFind.style.display = 'none';
-  } else {
-    modalFind.style.display = 'block';
+  }
+  if (e.target.classList.contains('nav-user_img--js')) {
+    if (modalFind.style.display == 'none') {
+      modalFind.style.display = 'block';
+    }
   }
 });
 document.addEventListener('click', function(e) {
-  if (!(e.target.tagName == 'INPUT' || e.target.tagName == 'FORM')) {
+  if (
+    !(
+      e.target.tagName == 'INPUT' ||
+      e.target.tagName == 'FORM' ||
+      e.target.tagName == 'BUTTON'
+    )
+  ) {
     modalLogin.style.display = 'none';
   }
 
   if (e.target.classList.contains('nav-user_enter')) {
     if (modalLogin.style.display == 'none') {
-      console.log(1);
       modalLogin.style.display = 'block';
-    } else {
-      console.log(2);
-      modalLogin.style.display = 'none';
     }
   }
 });
@@ -104,22 +108,39 @@ document.addEventListener('click', function(e) {
 //   }
 // });
 
-console.log(document.forms);
+for (let i = 0; i < document.forms.length; i++) {
+  document.forms[i].addEventListener('submit', function(e) {
+    e.preventDefault();
 
-document.forms[1].addEventListener('submit', function(e) {
-  e.preventDefault();
-  let form = new FormData(this);
-  let obj = {};
-  for (let [key, value] of form) {
-    obj = [key, value];
-  }
-  console.log(obj);
-  axios
-    .post('telegram.php', form)
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-});
+    let form = new FormData(this);
+    let obj = {};
+    for (let [name, value] of form) {
+      obj[name] = value;
+    }
+    for (let j = 0; j < document.forms[i].elements.length; j++) {
+      if (document.forms[i].elements[j].value == '') {
+        document.forms[i].elements[j].classList.add('error');
+        document.forms[i].elements[j].placeholder = 'заполните поле';
+      } else {
+        document.forms[i].elements[j].classList.remove('error');
+
+        axios
+          .post('telegram.php', obj)
+          .then(function(response) {
+            console.log(response);
+            modalLogin.style.display = 'none';
+            
+          })
+          .catch(function(error) {
+            console.log('ваша заявка не принята');
+          });
+      }
+    }
+    // document.forms[i].elements.value = 'aaaaa';
+    //}
+    //console.log(obj);
+    // if (obj.value == '') {
+    //   console.log('заполните поля ');
+    // }
+  });
+}
